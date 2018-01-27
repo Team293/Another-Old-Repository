cb8cc1321adf0ce25d0471c7a31abb9c52c9f9b9
FRC Team 293 Hopewell, NJ <293.First@gmail.com>
2018-01-27 09:49:12 -0500

    Made testing prgran for shooter

---
+++ src/org/usfirst/frc/team293/robot/commands/FeederRelease.java
@@ -0,0 +1,50 @@
+package org.usfirst.frc.team293.robot.commands;
+
+import org.usfirst.frc.team293.robot.OI;
+import org.usfirst.frc.team293.robot.Robot;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class FeederRelease extends Command {
+
+    public FeederRelease() {
+        // Use requires() here to declare subsystem dependencies
+    	requires(Robot.Feeder);
+    	}
+
+    	// Called just before this Command runs the first time
+    	@Override
+    	protected void initialize() {
+    	}
+
+    	// Called repeatedly when this Command is scheduled to run
+    	@Override
+    	protected void execute() {
+    		Robot.Feeder.move(-.5);
+    		Timer.delay(3);
+    		Robot.Feeder.move(0);
+    	}
+
+    	// Make this return true when this Command no longer needs to run execute()
+    	@Override
+    	protected boolean isFinished() {
+    		return true;
+    	}
+
+    	// Called once after isFinished returns true
+    	@Override
+    	protected void end() {
+    	
+    	}
+
+    	// Called when another command which requires one or more of the same
+    	// subsystems is scheduled to run
+    	@Override
+    	protected void interrupted() {
+    	//	new StopFeeder();
+    	}
+    }
